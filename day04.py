f08225904c728cf46f703236549652d85fcd9c7c
John Shiles <john.shiles@talentsconnect.com>
2024-12-05 10:41:00 +0100

    part 2 is done.

--- day04.py
+++ day04.py
@@ -3,6 +3,7 @@ import sys
 from collections.abc import Sequence
 from typing import Generator, Optional, Any
 
+
 def directions() -> Generator[tuple[int, int], Any, None]:
     """return all directions"""
     for x in [-1, 0, 1]:
@@ -11,7 +12,7 @@ def directions() -> Generator[tuple[int, int], Any, None]:
                 yield (x, y)
 
 
-def count_word_occurrences(grid, word) -> int:
+def count_word_occurrences(grid: list[list[str]], word: str) -> int:
     """
     Search grid for the number of occurrences of word. Diagonal, Hoirzonatal,
     Vertical, and reverse are allowed.
@@ -48,6 +49,29 @@ def count_word_occurrences(grid, word) -> int:
     return total_count
 
 
+def search_mas_in_x(grid: list[list[str]], word: str):
+    """Return the count of spaces where MAS forms an X, reversed is ok."""
+    rows, cols = len(grid), len(grid[0])
+
+    def is_valid(x, y):
+        """Check if the coordinates are within grid boundaries."""
+        return 0 <= x < rows and 0 <= y < cols
+
+    def validate_diag(x: int, y: int, direction: list[tuple[int,int]]) -> bool:
+        diag = "".join([grid[x+dx][y+dy] for dx, dy in direction if is_valid(x+dx,y+dy)])
+        if diag == word or diag[::-1] == word:
+            return True
+        return False
+
+    directions = [[(-1, -1), (0, 0), (1, 1)], [(-1, 1), (0, 0), (1, -1)]]
+    total_count = 0
+    for x in range(rows):
+        for y in range(cols):
+            if all([validate_diag(x, y, dir) for dir in directions]):
+                total_count += 1
+    return total_count
+
+
 def read_grid(file_path: str) -> list[list[str]]:
     """Read in our grid from a file"""
     with open(file_path, 'r') as file:
@@ -62,12 +86,12 @@ def parse_args(argv: Optional[Sequence[str]] = None) -> argparse.Namespace:
 
 def main(argv: Optional[Sequence[str]] = None) -> None:
     """
-    https://adventofcode.com/2024/day/3
+    https://adventofcode.com/2024/day/4
     """
     args = parse_args(argv)
     grid = read_grid(args.file)
-    word = "XMAS"
-    print(f"Part 1: {count_word_occurrences(grid, word)}")
+    print(f"Part 1: {count_word_occurrences(grid, 'XMAS')}")
+    print(f"Part 2: {search_mas_in_x(grid, 'MAS')}")
 
 
 if __name__ == "__main__":
